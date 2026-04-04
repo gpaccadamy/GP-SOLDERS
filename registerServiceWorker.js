a0358411ca805f685bffd3916230258d59a03951
gpaccadamy <gpaccadamy@gmail.com>
2026-04-04 21:01:38 +0530

    Delete registerServiceWorker.js

--- registerServiceWorker.js
+++
@@ -1,8 +0,0 @@
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/GP-SOLDERS/service-worker.js")
-      .then(reg => console.log("Service Worker registered:", reg))
-      .catch(err => console.error("SW registration failed:", err));
-  });
-}
